78692971f4b628ab1193aaf201b8e13890aab763
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-08-13 12:43:10 +0530

    Update Three.java

--- Three.java
+++ Three.java
@@ -9,10 +9,23 @@ class Three
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		double n1=1, n2=2, n3=3;
-        if( n1 >= n2 && n1 >= n3)
-        System.out.println("1");
-       else if(n2 >= n1 && n2 >=n3)
-       System.out.println("2");
-       else
-       System.out.println("3");
+        int a,b,c;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+	c=sc.nextInt();
+       if((a>b)&&(b>c))
+       {
+	       System.out.println(a);
+       }
+		else if((b>a)&&(b>c))
+		{
+			System.out.println(b);
+		}
+		else
+		{
+			System.out.println(c);
+		}
+	}
+}
+     
