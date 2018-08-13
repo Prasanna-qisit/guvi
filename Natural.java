3244d67295d592defd5a5300955144cc4abe0aac
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-08-13 16:03:55 +0530

    Create Natural.java

---
+++ Natural.java
@@ -0,0 +1,20 @@
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Natural
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	 int n,sum=0;
+     Scanner sc=new Scanner(System.in);
+     n=sc.nextInt();
+     for(int i=0;i<=n;i++)
+{
+    sum=sum+i;
+  
+} 
+    System.out.println(sum);
+	}
+}
