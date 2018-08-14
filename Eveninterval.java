278f00365fe58174aeebec2d4b4a82e73840ae6a
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-08-14 13:03:10 +0530

    Update and rename Even.java to Eveninterval.java

---
+++ Eveninterval.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Eveninterval
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int n,i;
+        Scanner sc=new Scanner(System.in);
+       n=sc.nextInt();
+       i=2;
+while(i<=n)
+{
+      
+i=i+2;
+}
+System.out.println(i +"\t");
+	}
+}
