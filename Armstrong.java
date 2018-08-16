6af5f2a3eef358fac7899816617e8ad0e4b47d1b
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-08-16 09:57:59 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,21 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Armstrong
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int low=0,high;{
+boolean flag=false;
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=2;i<=low/2;++i)
+{
+if(!flag)
+System.out.println(low+ " ");
+++low;
+}}
