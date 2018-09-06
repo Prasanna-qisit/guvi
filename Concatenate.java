3bede23dcd9e63fbcd2d77ddb4c1da3623a23460
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 10:11:45 +0530

    Create Concatenate.java

---
+++ Concatenate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Concatenate
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str1=sc.next();
+		String str2=sc.next();
+		String str3=str1+str2;
+		System.out.println(str3);
+		
+	}
+}
