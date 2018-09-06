f982f57b9b1987e438fe4273ddd1802302c7990b
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 15:07:19 +0530

    Create SumNatural.java

---
+++ SumNatural.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SumNatural
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		for(int i=1;i<=n;i++)
+		{
+			sum=sum+i;
+		}
+		System.out.println(sum);
+	}
+}
