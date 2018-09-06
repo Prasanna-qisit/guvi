fbe620a166ff468af9db0fdc49b662f9b9c0f0f7
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 14:21:42 +0530

    Create Avg.java

---
+++ Avg.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Avg
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int sum=0,aver;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for( int i=0;i<n;i++)
+		{
+		  arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+arr[i];
+		}
+		aver=sum/n;
+		System.out.println(aver);
+		
+	
+	}
+}
