814383eb82ec521833e561c83224ac8cef96c526
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 12:13:26 +0530

    Create Power2.java

---
+++ Power2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Power2
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(power(n))
+		System.out.println("yes");
+		else
+		System.out.println("no");
+	}
+	public static boolean power(int n)
+	{
+		if(n==0)
+		return false;
+		while(n!=1)
+		{
+			if(n%2!=0)
+			return false;
+			n=n/2;
+		}
+		return true;
+	}
+}
