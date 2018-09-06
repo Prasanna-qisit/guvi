702fd7eac195f284c2e49426386f0e60afc3e390
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 09:57:05 +0530

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Fibonacci
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int c;
+		int a=0,b=1;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(b);
+		for(int i=1;i<n;i++)
+		{
+			 c=a+b;
+			 System.out.println(c);
+			a=b;
+			b=c;
+		}
+		
+		
+		
+	}
+}
