ebb63011b43724671165ec4d6d2c919b58eab516
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 15:15:41 +0530

    Create CheckNum.java

---
+++ CheckNum.java
@@ -0,0 +1,33 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CheckNum
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		int count=0;
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int k=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++)
+		{
+			if(a[i]==k)
+			{
+			count++;
+			}
+		}
+		if(count>0)
+		{
+			System.out.println("yes");
+		}
+		else
+		{
+			System.out.println("no");
+		}
+	}
+}
