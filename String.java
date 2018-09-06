f66676430e2034eefa737dfb44107a7667b5e242
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 09:41:40 +0530

    Create String.java

---
+++ String.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class String
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int n=sc.nextInt();
+		for(int i=0;i<n;i++)
+		System.out.println(str);
+	}
+}
