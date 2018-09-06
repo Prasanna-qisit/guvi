44db168a8710081ccc79b62e4fbdd99b57f73950
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 16:01:29 +0530

    Create SemiColon.java

---
+++ SemiColon.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SemiColon
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		if(System.out.printf(str)==null)
+		{
+		}
+	}
+}
