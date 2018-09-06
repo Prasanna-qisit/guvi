dee32a00caca9293dd41d07177a4e34ec0db33d6
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 15:31:11 +0530

    Create SubStr.java

---
+++ SubStr.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class SubStr
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int n=sc.nextInt();
+		System.out.println(str.substring(0,n));
+	}
+}
