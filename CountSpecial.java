5b2097a6b8e6116cb3e073fb91415cddfffb04f4
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 10:22:47 +0530

    Create CountSpecial.java

---
+++ CountSpecial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CountSpecial
+{
+	public static void main (String[] args) 
+	{
+		int ch=0;
+		Scanner sc=new Scanner(System.in);
+		String str= sc.nextLine();
+		for(int i=0;i<str.length();i++)
+		{
+			if(!(Character.isAlphabetic(str.charAt(i))) && !(Character.isDigit(str.charAt(i))))
+			
+				ch++;
+			
+		}
+		System.out.println(ch);
+	}
+}
