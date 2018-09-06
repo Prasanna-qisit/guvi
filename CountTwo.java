e0e5ee2b1f09652d18c923d051efd1ed55f2418e
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-06 12:40:34 +0530

    Create CountTwo.java

---
+++ CountTwo.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CountTwo
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int r=n*2;
+		System.out.println(r);
+	}
+}
