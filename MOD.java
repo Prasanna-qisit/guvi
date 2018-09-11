1bc5ab96dc8243bcb25ea29368a2c083f40ed845
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-11 10:21:46 +0530

    Create MOD.java

---
+++ MOD.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class MOD
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int b=sc.nextInt();
+		int c=sc.nextInt();
+		int r=a*b%c;
+		System.out.println(r);
+	}
+}
