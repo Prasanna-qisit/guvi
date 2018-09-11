b5e8762d732032e7411f5e52b99ef2e0bad460a1
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-11 12:48:17 +0530

    Create :rogram105.java

---
+++ :rogram105.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Program105
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int i=sc.nextInt();
+		int j=sc.nextInt();
+		String k=""+i;
+		k+=j;
+		System.out.println(k);
+	}
+}
