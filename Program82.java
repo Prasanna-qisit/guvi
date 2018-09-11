6dd87d7b73be99c32a91e249b190b4d52268fb57
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-11 09:23:50 +0530

    Create Program82.java

---
+++ Program82.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Program82
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		float a=sc.nextFloat();
+		float b=sc.nextFloat();
+		float c=a*b;
+		System.out.printf("%.5f",c);
+	}
+}
