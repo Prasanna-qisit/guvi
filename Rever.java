94300c1d8a48c8207af322b68238b96ace3340ed
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-14 15:40:08 +0530

    Create Rever.java

---
+++ Rever.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Rever
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		String rev="";
+		for(int i=str.length()-1;i>=0;i--)
+	    {
+	      rev=rev+str.charAt(i);	
+	    }
+	    System.out.println(rev);
+	}
+}
