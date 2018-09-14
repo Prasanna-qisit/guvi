32dfa60c50b0ba074dc0f4cc728f5d69f51b0bac
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-14 14:28:15 +0530

    Create CeliusKelvin.java

---
+++ CeliusKelvin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class CeliusKelvin
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int c=sc.nextInt();
+		int k=c+273;
+		System.out.println(k);
+	}
+}
