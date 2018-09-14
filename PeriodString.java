156468d6120293b3630edb15ff0563a0eb5271c7
Prasanna-qisit <42332567+Prasanna-qisit@users.noreply.github.com>
2018-09-14 15:50:20 +0530

    Create PeriodString.java

---
+++ PeriodString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class PeriodString
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		str+=".";
+		System.out.println(str);
+	}
+}
